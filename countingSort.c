0aebda73d5b4c4bd963c72a2a8964ed7658057c8
Mr-ari <arijitghosh1106@gmail.com>
2018-02-17 00:32:53 +0530

    final commit

--- countingSort.c
+++ countingSort.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 #include<conio.h>
 
-int *counting_sort(int *arr,int n);
+void counting_sort(int *arr,int n);
 void printArray(int *arr,int size);
 
 int main(){
-	int n,i;
+	int n,i,*a;
 	printf("Enter the number of items you want to enter:");
 	scanf("%d",&n);
-	int arr[n];
+	int arr[n];a = &arr[0];
 	printf("Enter the numbers now :\n");
 	for (i=0;i<n;i++) scanf("%d",&arr[i]);
 	printf("Entered Array is :");
 	printArray(&arr[0],n);
-	int *final = counting_sort(&arr[0],n);
 	printf("After sorting the array is :");
-	printArray(counting_sort(&arr[0],n),n);
+	counting_sort(a,n);
 	getch();
 	return 0;
 }
 
-int *counting_sort(int *arr,int n){
-	int hash[10]={0},final[n];int i=0;
-	for (;i<n;i++){
+void counting_sort(int *arr,int n){
+	int hash[10]={0},final[n],i;;
+	int *x=&final[0];
+	for (i=0;i<n;i++){
 		hash[*(arr+i)]++;
 	}
 	for (i=1;i<10;i++){
@@ -32,7 +32,7 @@ int *counting_sort(int *arr,int n){
 			final[hash[*(arr+i)]-1] = *(arr+i);
 			hash[*(arr+i)]--;
 	}
-	return &final[0];
+	printArray(x,n);
 }
 
 void printArray(int *arr,int size){
